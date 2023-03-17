a92d271b0a11e7d795ffb9a08108a736930f6137
Tode Temov <75773817+temov@users.noreply.github.com>
2023-03-17 16:11:49 +0100

    Add files via upload

--- Homework_BlogPost_app/routes/blog.routes.js
+++ Homework_BlogPost_app/routes/blog.routes.js
@@ -1,2 +1,30 @@
-import express from 'express';
+import express from "express";
 import  BlogController from "../controllers/blog.controller.js";
+
+const blogController  = new BlogController();
+
+const blogRouter = express.Router();
+
+blogRouter.get("/",async (req,res) => {
+
+const blogs = await blogController.listBlogs();
+
+res.send(blogs);
+
+
+})
+
+blogRouter.post("/",async (req,res) => {
+
+    const body = req.body;
+
+    if(!body.title || !body.body || !body.author||!body.date||!body.tags){
+        res.status(400).send({message: "Request body invalid"});
+        return;
+    }
+
+    await blogController.createBlog(body.title, body.body, body.author, body.date, body.tags);
+
+    res.status(201).send({message: "Car was created."})
+
+})
