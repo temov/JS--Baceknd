b4192bdf061e1c678163e6f770bfa7ca1eaea637
Tode Temov <75773817+temov@users.noreply.github.com>
2023-04-27 00:09:51 +0200

    Update app.controller.ts

--- food-order_app/src/app.controller.ts
+++ food-order_app/src/app.controller.ts
@@ -8,18 +8,11 @@ export class AppController {
   // orderService = new OrderService();
   constructor(
     private readonly appService: AppService,
-    private readonly orderService: OrderService
     ) {}
 //localhost:3000 - default route
   @Get() 
   getHello(): string {
     return this.appService.getHello();
   }
-   //localhost:3000/orders 
-  @Get("/orders")
-  getOrders() {
 
-    const orders = this.orderService.getOrders();
-    return orders;
-  }
 }
