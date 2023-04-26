e7283e0a73ceaf30837949d5c39edf80aa67b1aa
Tode Temov <75773817+temov@users.noreply.github.com>
2023-04-27 00:10:52 +0200

    Update app.module.ts

--- food-order_app/src/app.module.ts
+++ food-order_app/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
-import { OrderService } from './orders/orders.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
